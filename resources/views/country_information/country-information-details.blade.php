a6a1aa14ef7b620bbc8bd111db6aa0300e5da7c0
nicckk3 <35618468+nicckk3@users.noreply.github.com>
2019-08-23 19:06:35 +0800

    commit

--- resources/views/country_information/country-information-details.blade.php
+++ resources/views/country_information/country-information-details.blade.php
@@ -143,7 +143,36 @@ $contents = getCountryInformationBasedOnDetails($_GET['country'], $_GET['categor
 
                     @else
 
-                    @include('inc.signin')
+                        <div class="space-1">
+                            @include('admin.inc.message')
+                            <form class="form-wrap-1 form-type form-ani" role="form" method="POST" action="{{ url('/login') }}">
+                                {{ csrf_field() }}
+                                <h1 class="title-1 text-center">Sign in to your account</h1>
+                                <div class="inrow">
+                                    <span><input type="text" name="email" class="form-control" /></span>
+                                    <label>Email Address</label>
+                                </div>
+                                <div class="inrow">
+                                    <span><input type="password" name="password" class="form-control" /></span>
+                                    <label>Password</label>
+                                    <input type="hidden" name="redirect" value="{{ url()->full() }}">
+                                </div>
+                                <div class="form-group">
+                                    <div class="google-recaptcha">
+                                        <div class="g-recaptcha" data-callback="onSubmit"
+                                             data-sitekey="{{env('CAPTCHA_SITE_KEY')}}" data-size="invisible"></div>
+                                        <input type="hidden" title="Please verify this" class="required" name="keycode"
+                                               id="keycode">
+                                        <div id="cap-response" style="display:none; color:#F00;"></div>
+
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn-2">Sign in</button>
+                                <div class="links">
+                                    <a href="{{ route('password.request') }}">Forgot Password?</a>
+                                </div>
+                            </form>
+                        </div>
 
                     @endif
 
