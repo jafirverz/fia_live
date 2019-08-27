660b1fa294be860761587ae983255404972c8f09
Arvind Purohit <arvind.verz@gmail.com>
2019-08-27 18:23:33 +0530

    Issue 66, 74, 75, 72

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,4 @@
 <?php
 
-Route::get('/home', function () {
-    $users[] = Auth::user();
-    $users[] = Auth::guard()->user();
-    $users[] = Auth::guard('admin')->user();
-
-    //dd($users);
-
-    return view('admin.home');
-})->name('home');
+Route::get('/home', 'CMS\DashboardController@index');
 
