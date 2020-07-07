8ee110ad20f8859112212623d233ba8ceec6d85b
jafirverz <40601286+jafirverz@users.noreply.github.com>
2020-07-07 08:42:34 +0530

    sub

--- resources/views/admin/thinking-piece/create.blade.php
+++ resources/views/admin/thinking-piece/create.blade.php
@@ -13,7 +13,6 @@
         <!-- Small boxes (Stat box) -->
         <div class="row">
             <div class="col-lg-12">
-                @include('admin.inc.message')
                         <!-- general form elements -->
                     <!-- /.box-header -->
                     <div class="box box-primary">
@@ -21,26 +20,44 @@
 		<form name="filter" method="post" action="{{ url('/admin/thinking_piece/store')}}" enctype="multipart/form-data">
                     <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
                <div class="box-body">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('thinking_piece_title') ? ' has-error' : '' }}">
                 <div class="row">
                     <label class='col-sm-2 control-label'>Title:</label>
-    <div class="col-sm-10"><input type="text" class="form-control" name="thinking_piece_title" id="thinking_piece_title"  value="{{ old('thinking_piece_title') }}">
-                </div>
+    <div class="col-sm-10">
+    <input type="text" class="form-control" name="thinking_piece_title" id="thinking_piece_title"  value="{{ old('thinking_piece_title') }}">
+                            @if ($errors->has('thinking_piece_title'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('thinking_piece_title') }}</strong>
+                            </span>
+                            @endif
+    </div>
                 </div></div>
                 
-                <div class="form-group"><div class="row">                
+                <div class="form-group {{ $errors->has('thinking_piece_date') ? ' has-error' : '' }}"><div class="row">                
                     <label class='col-sm-2 control-label'>Date:</label>
 <div class="col-sm-10 date  pick_date_time"><input readonly="readonly" type="text" class="form-control" name="thinking_piece_date" id="thinking_piece_date" value="{{ old('thinking_piece_date') }}">
+                             @if ($errors->has('thinking_piece_date'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('thinking_piece_date') }}</strong>
+                            </span>
+                            @endif
                 <span class="add-on"><i class="icon-th"></i></span></div></div></div>
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('description') ? ' has-error' : '' }}">
                 <div class="row">
                     <label class='col-sm-2 control-label' for="contents">Description :</label>
 <div class="col-sm-10"><textarea class="tiny-editor form-control" rows="5" id="description"
-                              name="description">{{ old('description') }}</textarea></div>
+                              name="description">{{ old('description') }}</textarea>
+                              
+                            @if ($errors->has('description'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('description') }}</strong>
+                            </span>
+                            @endif
+                  </div>
                 </div>
                 </div>
                 
-                 <div class="form-group{{ $errors->has('thinking_piece_image') ? ' has-error' : '' }}">
+                 <div class="form-group {{ $errors->has('thinking_piece_image') ? ' has-error' : '' }}">
                            <div class="row">
                            <label for="thinking_piece_image" class="col-sm-2 control-label">Image</label>                            
                            <div class="col-sm-10"> <input type="file" name="thinking_piece_image" class="form-control" placeholder="" />
@@ -52,10 +69,17 @@
                             </div>
                            </div>
                         </div>           
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('thinking_piece_address') ? ' has-error' : '' }}">
                    <div class="row"> <label class='col-sm-2 control-label'>Address:</label>
  <div class="col-sm-10"><textarea class="tiny-editor form-control" rows="5" id="thinking_piece_address"
-                              name="thinking_piece_address">{{ old('thinking_piece_address') }}</textarea></div>
+                              name="thinking_piece_address">{{ old('thinking_piece_address') }}</textarea>
+                              
+                               @if ($errors->has('thinking_piece_address'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('thinking_piece_address') }}</strong>
+                            </span>
+                            @endif
+                              </div>
                 </div>
                </div></div>
                 <!-- /.box-body -->
