f05d5b717eda3edf4d0ba4cc9e020571a9730713
jafirverz <40601286+jafirverz@users.noreply.github.com>
2020-07-17 21:31:31 +0530

    sub

--- resources/views/thinking_piece/thinking_piece-details.blade.php
+++ resources/views/thinking_piece/thinking_piece-details.blade.php
@@ -5,7 +5,8 @@
 				@include('inc.banner')
 				<div class="container space-1">
 					<h1 class="title-1" style="margin-bottom: 0;">{{ $thinkingPiece->thinking_piece_title }}</h1>
-					<p class="date-post">{{date("jFY",strtotime($thinkingPiece->thinking_piece_date))}}  |  {{ date("h:i A",strtotime($thinkingPiece->thinking_piece_date)) }}</p>
+					<p class="date-post">{{date("j F, Y",strtotime($thinkingPiece->thinking_piece_date))}}  |  {{ date("h:i A",strtotime($thinkingPiece->thinking_piece_date)) }}</p>
+                    <p><p>{{$thinkingPiece->thinking_piece_address}}</p></p>
 					<article class="document">
 						<figure><img src="{{asset($thinkingPiece->thinking_piece_image)}}" alt="" /></figure>
 						{!! $thinkingPiece->description!!}
