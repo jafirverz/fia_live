2ed1e5a6ab6c61c747ea8673c3793e4747d9e6f5
nicckk3 <35618468+nicckk3@users.noreply.github.com>
2020-07-22 10:33:42 +0800

    newslatter update

--- resources/views/emails/regulatory-updates.blade.php
+++ resources/views/emails/regulatory-updates.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -12,7 +11,7 @@
         font-family: Arial !important;
     }
 
-    @media    only screen and (max-width: 600px) {
+    @media        only screen and (max-width: 600px) {
         .inner-body {
             width: 100% !important;
         }
@@ -22,7 +21,7 @@
         }
     }
 
-    @media    only screen and (max-width: 500px) {
+    @media        only screen and (max-width: 500px) {
         .button {
             width: 100% !important;
         }
