9608e6a9ec09a11f8d83b7727374558101005bdd
nicckk3 <35618468+nicckk3@users.noreply.github.com>
2020-08-13 17:09:08 +0800

    complete

--- resources/views/admin/podcast/create.blade.php
+++ resources/views/admin/podcast/create.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.layout.dashboard') @section('content')
-        <!-- Content Wrapper. Contains page content -->
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -14,94 +14,97 @@
         <div class="row">
             <div class="col-lg-12">
                 <!--@include('admin.inc.message')-->
-                        <!-- general form elements -->
-                    <!-- /.box-header -->
-                    <div class="box box-primary">
-                        <!-- form start -->
-		<form name="podcast" method="post" action="{{ url('/admin/podcast/store')}}" enctype="multipart/form-data">
-                    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-               <div class="box-body">
-               <div class="form-group{{ $errors->has('topical_id') ? ' has-error' : '' }}">
-                <label for="">Topic:</label>
-                <select name="topical_id[]" class="form-control select2" style="width: 100%;" multiple>
-                    <option value="">-- Select --</option>
-                    @if($topics)
-                    @foreach ($topics as $topic)
-                    <option value="{{ $topic->id }}" @if(old('topical_id')) @if(in_array($topic->id, old('topical_id'))) selected @endif @endif>{{ $topic->tag_name }}</option>
-                    @endforeach
-                    @endif
-                </select>
-                @if ($errors->has('topical_id'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('topical_id') }}</strong>
-                </span>
-                @endif
-                </div>
-               
-                <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                
-                    <label class='control-label'>Title:</label>
-    <input type="text" class="form-control" name="title" id="title"  value="{{ old('title') }}">
-               
-               @if ($errors->has('title'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('title') }}</strong>
-                </span>
-                @endif
-                </div>
-                
-                <div class="form-group {{ $errors->has('podcast_image') ? ' has-error' : '' }}">
+                <!-- general form elements -->
+                <!-- /.box-header -->
+                <div class="box box-primary">
+                    <!-- form start -->
+                    <form name="podcast" method="post" action="{{ url('/admin/podcast/store')}}"
+                        enctype="multipart/form-data">
+                        <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                        <div class="box-body">
+                            <div class="form-group{{ $errors->has('topical_id') ? ' has-error' : '' }}">
+                                <label for="">Topic:</label>
+                                <select name="topical_id[]" class="form-control select2" style="width: 100%;" multiple>
+                                    <option value="">-- Select --</option>
+                                    @if($topics)
+                                    @foreach ($topics as $topic)
+                                    <option value="{{ $topic->id }}" @if(old('topical_id')) @if(in_array($topic->id,
+                                        old('topical_id'))) selected @endif @endif>{{ $topic->tag_name }}</option>
+                                    @endforeach
+                                    @endif
+                                </select>
+                                @if ($errors->has('topical_id'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('topical_id') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+
+                                <label class='control-label'>Title:</label>
+                                <input type="text" class="form-control" name="title" id="title"
+                                    value="{{ old('title') }}">
+
+                                @if ($errors->has('title'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('title') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group {{ $errors->has('podcast_image') ? ' has-error' : '' }}">
                                 <label for="podcast_image" class=" control-label">Image:</label>
 
                                 <div class="">
-                                    <input type="file" name="podcast_image" class="form-control" placeholder=""/>
-                @if ($errors->has('podcast_image'))
-                			<span class="help-block">
-                               <strong>{{ $errors->first('podcast_image') }}</strong>
-     						</span>
-                @endif
+                                    <input type="file" name="podcast_image" class="form-control" placeholder="" />
+                                    @if ($errors->has('podcast_image'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('podcast_image') }}</strong>
+                                    </span>
+                                    @endif
                                     <p class="text-muted"><strong>Note:</strong>
-                                            Image size should be 600*600 and not more than 2MB for better display.
+                                        Image size should be 600*600 and not more than 2MB for better display.
                                     </p>
                                 </div>
                             </div>
-                <div class="form-group {{ $errors->has('audio_file') ? ' has-error' : '' }}">
-                            <label for="audio_file" class=" control-label">Audio File:</label>                            
-                            
+                            <div class="form-group {{ $errors->has('audio_file') ? ' has-error' : '' }}">
+                                <label for="audio_file" class=" control-label">Audio File:</label>
+
                                 <input type="file" name="audio_file" class="form-control" placeholder="" />
                                 <p class="text-muted"><strong>Note:</strong> Only application/octet-stream,
                                     audio/mpeg, mpga, mp3, wav file and not more than 50MB.
                                 </p>
-                 @if ($errors->has('audio_file'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('audio_file') }}</strong>
-                </span>
-                @endif
-                        </div>                    
-                <div class="form-group {{ $errors->has('description') ? ' has-error' : '' }}">
-                
-                    <label class='control-label' for="contents">Description :</label>
-<textarea class="tiny-editor form-control" rows="5" id="description"
-                              name="description">{{ old('description') }}</textarea>
-                  @if ($errors->has('description'))
-                			<span class="help-block">
-                               <strong>{{ $errors->first('description') }}</strong>
-     						</span>
-                @endif            
-                </div>
-                
-            </div>
-                <!-- /.box-body -->
-                <div class="box-footer">
-                    <button type="submit" class="btn btn-info pull-right"><i class="fa  fa-check"></i> Add
-                    </button>
-                </div>
-                <!-- /.box-footer -->
-		</form>
+                                @if ($errors->has('audio_file'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('audio_file') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                            <div class="form-group {{ $errors->has('description') ? ' has-error' : '' }}">
 
-</div>
-                    <!-- /.box-body -->
+                                <label class='control-label' for="contents">Description :</label>
+                                <textarea class="tiny-editor form-control" rows="5" id="description"
+                                    name="description">{{ old('description') }}</textarea>
+                                @if ($errors->has('description'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('description') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+
+                        </div>
+                        <!-- /.box-body -->
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-info pull-right"><i class="fa  fa-check"></i> Add
+                            </button>
+                        </div>
+                        <!-- /.box-footer -->
+                    </form>
+
                 </div>
+                <!-- /.box-body -->
+            </div>
         </div>
         <!-- /.row -->
     </section>
@@ -109,8 +112,7 @@
 </div>
 
 <script>
-	$('#audio_file').filemanager('image');
+    $('#audio_file').filemanager('image');
 </script>
 <!-- /.content-wrapper -->
 @endsection
-
